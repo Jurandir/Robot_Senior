c5a1385558b5b9283194b4f3bd226a6f9c4c3da6
Jurandir Brito Ferreira Junior <jurandir.junior@termaco.com.br>
2022-01-26 18:29:30 -0300

    Ajustes/SQL/DePara CF/

--- src/controllers/IT/valida_idCargaPK.js
+++ src/controllers/IT/valida_idCargaPK.js
@@ -10,7 +10,7 @@ const logEventos      = require('../../helpers/logEventos')
 
 const valida_idCargaPK = async (cfg) => {                                                
     let sql = `
-    SELECT TOP 50 DF.* ,TK.TOKEN
+    SELECT TOP 100 DF.* ,TK.TOKEN
 	,(SELECT TOP 1 1 FROM SIC.dbo.ITRACK_CLIENTE CL 
 	  WHERE (CL.RAIZ_CNPJ=SUBSTRING(DF.EMBARCADOR,1,8) 
 	     OR CL.RAIZ_CNPJ=SUBSTRING(DF.DESTINATARIO,1,8))
@@ -18,10 +18,11 @@ const valida_idCargaPK = async (cfg) => {
     FROM SIC.dbo.ITRACK_DANFE DF
     JOIN SIC.dbo.ITRACK_TOKEN TK ON TK.CNPJ = DF.TRANSPORTADOR
    WHERE DF.IDCARGA = 0
+     AND (DATEDIFF(day, DF.DT_EMISSAO,   CURRENT_TIMESTAMP) < 30 )                              ---Pesquisa até 30 dias apos a data da emissão 
      AND ( DF.DT_UPDATE    IS NULL OR DATEDIFF(minute,DF.DT_UPDATE   , CURRENT_TIMESTAMP) > 30) ---UPDATE    depois de 30 min da ultima tentativa
      AND ( DF.DT_VALIDACAO IS NULL OR DATEDIFF(minute,DF.DT_VALIDACAO, CURRENT_TIMESTAMP) > 30) ---VALIDACAO depois de 30 min da ultima tentativa
-     AND ( DF.DT_ENTREGA   IS NULL OR DATEDIFF(MONTH, DF.DT_ENTREGA,   CURRENT_TIMESTAMP) < 10) ---Pesquisa até 10 dias apos a data da entrega     
-     
+     AND ( DF.DT_ENTREGA   IS NULL OR DATEDIFF(day, DF.DT_ENTREGA,   CURRENT_TIMESTAMP) < 15)   ---Pesquisa até 15 dias apos a data da entrega     
+
      ORDER BY DF.DT_EMISSAO DESC
      `
     try {
